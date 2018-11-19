c2a376c410361025ae67f3685975ad53effa88c6
early501bird <early501bird@126.com>
2018-11-19 17:06:12 +0800

    create models

--- djangoProject/App1/models.py
+++ djangoProject/App1/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Grades(models.Model):
+    name = models.CharField(max_length=20)
+    date = models.DateField()
+    girlnum = models.IntegerField()
+    boynum = models.IntegerField()
+    isDelete = models.BooleanField(default=False)
+
+
+class Students(models.Model):
+    name = models.CharField(max_length=20)
+    gender = models.BooleanField(default="True")
+    age = models.IntegerField()
+    content = models.CharField(max_length=100)
+    isDelete = models.BooleanField(default=False)
+
+    #关联外键
+    grade = models.ForeignKey('Grades')
